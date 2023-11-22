a204a0a59c58db1f48e7ffc4843f7668f9692a36
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-11-22 08:51:51 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsAlura/Form_HelloWorld.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsAlura
+{
+    partial class Form_HelloWorld
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Titulo = new System.Windows.Forms.Label();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Titulo
+            // 
+            this.label_Titulo.AutoSize = true;
+            this.label_Titulo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Titulo.Location = new System.Drawing.Point(30, 30);
+            this.label_Titulo.Name = "label_Titulo";
+            this.label_Titulo.Size = new System.Drawing.Size(255, 24);
+            this.label_Titulo.TabIndex = 0;
+            this.label_Titulo.Text = "Visual Studio .Net Version";
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Location = new System.Drawing.Point(609, 405);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(179, 33);
+            this.btn_Sair.TabIndex = 1;
+            this.btn_Sair.Text = "Fechar a aplicação";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
+            // Form_HelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Sair);
+            this.Controls.Add(this.label_Titulo);
+            this.Name = "Form_HelloWorld";
+            this.Text = "Hello World";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Titulo;
+        private System.Windows.Forms.Button btn_Sair;
+    }
+}
+
---
+++ WindowsFormsAlura/Form_HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAlura
+{
+    public partial class Form_HelloWorld : Form
+    {
+        public Form_HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
