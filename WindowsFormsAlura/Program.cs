a4962282082f05a9d8e0c981c6c96b79caaf2983
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-11-22 22:22:46 -0300

    VerificarSenha

--- WindowsFormsAlura/Program.cs
+++ WindowsFormsAlura/Program.cs
@@ -17,7 +17,8 @@ namespace WindowsFormsAlura
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form_HelloWorld());
-            Application.Run(new Form_DemonstracaoKey());
+            // Application.Run(new Form_DemonstracaoKey());
+            Application.Run(new Form_ValidaSenha());
 
         }
     }
