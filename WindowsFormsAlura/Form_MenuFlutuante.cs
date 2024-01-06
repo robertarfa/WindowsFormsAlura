c8f95c72030922635895a6b8cc9ca41e1312d351
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2024-01-06 15:41:31 -0300

    Menu E SubMenu flutuante

--- WindowsFormsAlura/Form_MenuFlutuante.cs
+++ WindowsFormsAlura/Form_MenuFlutuante.cs
@@ -27,9 +27,38 @@ namespace WindowsFormsAlura
                 var posicaoX = e.X;
                 var posicaoY = e.Y;
 
-                MessageBox.Show(e.Button.ToString() + " posicaoX: " + posicaoX + " posicaoY: " + posicaoY);
+                var contextMenu = new ContextMenuStrip();
+
+                var menu1 = DesenhaItemMenu("Item do Menu 1");
+                var menu2 = DesenhaItemMenu("Item do Menu 2");
+
+                var subMenu1 = DesenhaItemMenu("SubMenu 1");
+                var subMenu2 = DesenhaItemMenu("SubMenu 2");
+
+                contextMenu.Items.Add(menu1);
+                contextMenu.Items.Add(menu2);
+
+                menu1.DropDownItems.Add(subMenu1);
+                menu2.DropDownItems.Add(subMenu2);
+
+
+                contextMenu.Show(this, new Point(posicaoX, posicaoY));
+
+
+                //MessageBox.Show(e.Button.ToString() + " posicaoX: " + posicaoX + " posicaoY: " + posicaoY);
+            }
+
+            ToolStripMenuItem DesenhaItemMenu(string text)
+            {
+                var vToolTip = new ToolStripMenuItem();
+
+                vToolTip.Text = text;
+
+                return vToolTip;
             }
 
         }
+
+
     }
 }
